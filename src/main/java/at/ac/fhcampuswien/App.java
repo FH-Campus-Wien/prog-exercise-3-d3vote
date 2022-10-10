94fe6827f576555dc0b4d1ef7eb3f26e35f1842c
d3vote <fedgamesyt@gmail.com>
2022-10-10 11:58:23 +0200

    Lesson commit

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,12 +5,30 @@ import java.util.Scanner;
 
 public class App {
 
-    // Implement all methods as public static
+    public static void oneMonthCalendar(int days, int start) {
+        System.out.println(days);
+        System.out.println(start);
+        int []numbers = {2, 3, 5, 6};
+        int []numbers2 = new int[10];
+        numbers2[0] = 6;
+        numbers2[1] = 4;
+
+        numbers2[9] = 9;
+        for (int i = 0; i < numbers2.length; i++){
+            System.out.println(numbers2[i]);
+        }
+    }
+
+    public static long[] lcg(long seed){
+        long[] randomNumbers;
+        return null;
+    }
 
     public static void main(String[] args) {
-        // test your method implementations here
-        // make method calls
-        // print their results
-        // etc.
+        Scanner sc = new Scanner(System.in);
+        int days = sc.nextInt();
+        int start = sc.nextInt();
+        oneMonthCalendar(days, start);
+        long[] randomNumbers = lcg(0);
     }
 }
